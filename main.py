ab30262cac7b4c7514a69e494aeace2b8698084f
OnkarGarg <onkargarg6@gmail.com>
2024-12-26 18:39:36 +0100

    Added learning modes

--- main.py
+++ main.py
@@ -8,27 +8,54 @@ with open('sentences.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
     sentences = data["sentences"]
     random.shuffle(sentences)
+    mode = input("Choose mode: 1 for Norwegian prompts requiring spoken norwegian,"
+                 " 2 for English prompts requiring spoken norwegian: ")
+    mode = int(mode) % 2
+    print("Mode: " + str(mode))
 
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
 
-        for sentence in sentences:
-            print(sentence["norwegian"])
-            audio = r.listen(source)
-            heard = ""
-            while not heard:
-                try:
-                    heard = r.recognize_google(audio, language="no-NO")
-                except sr.UnknownValueError:
-                    print("Google Speech Recognition could not understand audio")
-                    r.adjust_for_ambient_noise(source)
-                    audio = r.listen(source)
-                except sr.RequestError as e:
-                    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        if mode == 0:
+            for sentence in sentences:
+                print(sentence["norwegian"])
+                audio = r.listen(source)
+                heard = ""
+                while not heard:
+                    try:
+                        heard = r.recognize_google(audio, language="no-NO")
+                    except sr.UnknownValueError:
+                        print("Google Speech Recognition could not understand audio")
+                        r.adjust_for_ambient_noise(source)
+                        audio = r.listen(source)
+                    except sr.RequestError as e:
+                        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-            print("You said: " + heard)
-            if heard.lower() == sentence["norwegian"].lower().replace(".", "").replace("?", "").replace("!", "").replace(",", ""):
-                print("Correct!")
-            else:
-                print("Incorrect!")
+                print("You said: " + heard)
+                if heard.lower() == sentence["norwegian"].lower().replace(".", "").replace("?", "").replace("!", "").replace(",", ""):
+                    print("Correct!")
+                else:
+                    print("Incorrect!")
+        else:
+            for sentence in sentences:
+                print(sentence["english"])
+                audio = r.listen(source)
+                heard = ""
+                while not heard:
+                    try:
+                        heard = r.recognize_google(audio, language="no-NO")
+                    except sr.UnknownValueError:
+                        print("Google Speech Recognition could not understand audio")
+                        r.adjust_for_ambient_noise(source)
+                        audio = r.listen(source)
+                    except sr.RequestError as e:
+                        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+                print("You said: " + heard)
+                if heard.lower() == sentence["norwegian"].lower().replace(".", "").replace("?", "").replace("!",
+                                                                                                            "").replace(
+                        ",", ""):
+                    print("Correct!")
+                else:
+                    print("Incorrect!")
 
